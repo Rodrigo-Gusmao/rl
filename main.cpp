63c05dc61612ac4b96833b1d5ed6c85828d1fdb4
unknown <contatorodrigogusmao@gmail.com>
2026-05-28 22:58:47 -0300

    wip: determinantes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 using matrixf = vector<vector<float>>;
 
+void matrixPrint(matrixf vec){
+    if(vec.empty()) cout<<"empty.";
+
+    for(size_t row = 0; row < vec.size(); row++){
+        for(size_t col = 0; col < vec[row].size(); col++){
+            cout<<vec[row][col]<<" ";
+        }
+        cout<<"\n";
+    }
+    cout<<"end.\n";
+}
+
 matrixf matrixTranspose(matrixf matrix){
     if (matrix.empty()) return {};
 
@@ -18,7 +30,7 @@ matrixf matrixTranspose(matrixf matrix){
     return matrixt;
 }
 
-matrixf matrixMult(matrixf a, matrixf b){
+matrixf matrixMultiplication(matrixf a, matrixf b){
     if(a.empty() || b.empty()) return {};
 
     size_t aRows = a.size();
@@ -28,61 +40,64 @@ matrixf matrixMult(matrixf a, matrixf b){
 
     if(aCols != bRows) return {};
 
-    matrixf resultant(bCols, vector<float>(aRows, 0.0f));
-/*
-A0 * Bx + A1 * Bx
-00 * 0x + 01 * 0x
-00 * 0x + 01 * 0x
-00 * 0x + 01 * 0x
-00 * 0x + 01 * 0x
------------------
-10 * 1x + 11 * 1x
-10 * 1x + 11 * 1x
-10 * 1x + 11 * 1x
-10 * 1x + 11 * 1x
-*/
-    for(size_t row = 0; row < aRows; row++){
-        for(size_t col = 0; col < bCols; col++){
-            cout<<a[row][0]<<"*"<<b[row][col]<<" + "<<a[row][1]<<"*"<<b[row][col]<<"\n";
+    matrixf resultant(aRows, vector<float>(bCols, 0.0f));
+
+    for (size_t row = 0; row < aRows; row++) {
+        for (size_t col = 0; col < bCols; col++) {
+            for (size_t n = 0; n < aCols; n++) {
+                resultant[row][col] = a[row][n] * b[n][col];
+            }
         }
     }
 
     return resultant;
 }
 
-void matrixPrint(matrixf vec){
-    if( vec.empty() ) cout<<"empty";
+int matrixDeterminant(matrixf matrix){
+    int rows = matrix.size();
+    int cols = matrix[0].size();
 
-    for(size_t row = 0; row < vec.size(); row++){
-        for(size_t col = 0; col < vec[row].size(); col++){
-            cout<<vec[row][col]<<" ";
-        }
-        cout<<"\n";
+    if(rows != cols) return 0;
+
+    if(rows == 2){
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][1];
     }
-    cout<<"\n";
+
+    /* TEOREMA DE LAPLACE
+    C[i][j] = -1^i+j * det(A[i][j])
+    
+    det(A) = a[i][1]*C[i][1] + a[i][2]*C[i][2] + ... + a[i][n]*C[i][n]
+    ou
+    det(A) = a[1][j]*C[1][j] + a[2][j]*C[2][j] + ... + a[n][j]*C[n][j]
+    */
+    
+}
+
+matrixf matrixInversion(matrixf matrix){
+    int det = matrixDeterminant(matrix);
+    if(det == 0) return {};
 }
 
 int main(){
     matrixf a = {
-        {500,200},
-        {800,300}
+        {500,200,100,150},
+        {800,300,200,225}
     };
     matrixf b = {
-        {1,2,1,3},
-        {1,1,2,1}
+        {1,2,1,3,1},
+        {1,1,2,1,2},
+        {2,1,3,1,3},
+        {1,2,3,2,4}
     };
-/*
-500 * 1 + 200 * 1
-500 * 2 + 200 * 2
-500 * 1 + 200 * 1
-500 * 3 + 200 * 3
------------------
-800 * 1 + 300 * 1
-800 * 1 + 300 * 1
-800 * 2 + 300 * 2
-800 * 1 + 300 * 1
-*/
-    matrixf mult = matrixMult(a,b);
+
+    matrixf transA = matrixTranspose(a);
+    matrixf transB = matrixTranspose(b);
+    matrixPrint(a);
+    matrixPrint(transA);
+    matrixPrint(b);
+    matrixPrint(transB);
+
+    matrixf mult = matrixMultiplication(a,b);
     matrixPrint(mult);
 
     return 0;
